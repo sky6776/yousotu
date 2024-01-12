1f93dfcee89a04574bc0a1cb8ecbf957e5f015b6
sky_6776 <137915295+sky6776@users.noreply.github.com>
2024-01-12 11:46:09 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+      document.getElementById('opening-layer').style.opacity = '0';
+      setTimeout(function() {
+          document.getElementById('opening-layer').style.display = 'none';
+      }, 3000);
+  }, 1000);
+});
