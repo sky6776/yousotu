88114c4b49900981247a047616b159015ccc0856
sky_6776 <137915295+sky6776@users.noreply.github.com>
2024-01-16 13:21:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+  const categoryButtons = document.querySelectorAll('.category-button');
+  const products = document.querySelectorAll('.product');
+
+  categoryButtons.forEach(button => {
+      button.addEventListener('click', function() {
+          const category = this.getAttribute('data-category');
+
+          categoryButtons.forEach(btn => btn.classList.remove('active'));
+          this.classList.add('active');
+
+          products.forEach(product => {
+              product.style.display = 'none';
+              if (category === 'all' || product.classList.contains(category)) {
+                  product.style.display = 'block';
+              }
+          });
+      });
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
       document.getElementById('opening-layer').style.opacity = '0';
       setTimeout(function() {
