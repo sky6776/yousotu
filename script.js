31436103af5f2301aac37653d27daf815dbbed5a
sky_6776 <137915295+sky6776@users.noreply.github.com>
2024-01-26 13:00:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  const categoryButtons = document.querySelectorAll('.category-button');
-  const products = document.querySelectorAll('.product');
-
-  categoryButtons.forEach(button => {
-      button.addEventListener('click', function() {
-          const category = this.getAttribute('data-category');
-
-          categoryButtons.forEach(btn => btn.classList.remove('active'));
-          this.classList.add('active');
-
-          products.forEach(product => {
-              product.style.display = 'none';
-              if (category === 'all' || product.classList.contains(category)) {
-                  product.style.display = 'block';
-              }
-          });
-      });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-
     setTimeout(function() {
         document.getElementById('opening-layer').style.opacity = '0';
         setTimeout(function() {
@@ -44,14 +22,3 @@ document.addEventListener("DOMContentLoaded", function() {
       }, 3000);
   }, 1000);
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    const soldOutProducts = document.querySelectorAll('.product.sold-out');
-
-    soldOutProducts.forEach(product => {
-      const soldOutOverlay = document.createElement('div');
-      soldOutOverlay.classList.add('sold-out-overlay');
-      soldOutOverlay.innerHTML = '<p>在庫切れ</p>';
-      product.appendChild(soldOutOverlay);
-    });
-  });
