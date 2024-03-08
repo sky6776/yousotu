f5fa3ee2920006cadbda09c84d71131801a73496
sky_6776 <137915295+sky6776@users.noreply.github.com>
2024-03-08 10:10:11 +0900

    Update script.js

--- witherskull/script.js
+++ witherskull/script.js
@@ -1,1 +1,5 @@
-
+function updatePrice(quantity) {
+    const pricePerUnit = 64;
+    const totalPrice = pricePerUnit * quantity;
+    document.querySelector('.product-price').textContent = 'レッドストーン' + totalPrice.toLocaleString();
+}
